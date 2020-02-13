64ca422e0fa2ff3619d041fea45ff78da21537e1
Anthony Fieroni <bvbfan@abv.bg>
2020-02-13 17:41:28 +0200

    Functional tests to pass
    Signed-off-by: Anthony Fieroni <bvbfan@abv.bg>

--- test/functional/rpc_blockchain.py
+++ test/functional/rpc_blockchain.py
@@ -127,11 +127,11 @@ class BlockchainTest(BitcoinTestFramework):
         assert_greater_than(res['size_on_disk'], 0)
 
         assert_equal(res['softforks'], {
-            'bip34': {'type': 'buried', 'active': False, 'height': 500},
+            'bip34': {'type': 'buried', 'active': False, 'height': 1000},
             'bip66': {'type': 'buried', 'active': False, 'height': 1251},
             'bip65': {'type': 'buried', 'active': False, 'height': 1351},
-            'csv': {'type': 'buried', 'active': False, 'height': 432},
-            'segwit': {'type': 'buried', 'active': True, 'height': 0},
+            'csv': {'type': 'buried', 'active': True, 'height': 0},
+            'segwit': {'type': 'buried', 'active': True, 'height': 150},
             'testdummy': {
                 'type': 'bip9',
                 'bip9': {
